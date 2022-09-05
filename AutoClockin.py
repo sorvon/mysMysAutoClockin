8966e1f06c8f640389971cda3b3de0749697465b
sorvon <45837973+sorvon@users.noreply.github.com>
2022-09-05 23:36:52 +0800

    Update AutoClockin.py

--- AutoClockin.py
+++ AutoClockin.py
@@ -27,8 +27,8 @@ class ConfMeta(type):
 
   @property
   def ua(self):
-    return 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) ' \
-           'AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/%s' %(self.app_version)
+    return 'Mozilla/5.0 (Linux; Android 11; Mi 10 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.73 Mobile Safari/537.36' \
+           ' miHoYoBBS/%s' %(self.app_version)
 
 
 class Conf(metaclass=ConfMeta):
