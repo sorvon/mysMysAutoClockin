a543116e3a4b07de2012066368b5227ddc37a2aa
sorvon <sorvon@qq.com>
2022-12-29 01:42:45 +0800

    Update v2freeClockin.py

--- v2freeClockin.py
+++ v2freeClockin.py
@@ -5,6 +5,10 @@ import logging
 import argparse
 import requests
 
+logging.basicConfig(
+  level = logging.INFO,
+  format = '%(asctime)s %(levelname)s %(message)s',
+  datefmt = '%Y-%m-%dT%H:%M:%S')
 
 class CheckIn(object):
     client = requests.Session()
